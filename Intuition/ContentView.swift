1f1d45bb0a9ccdcca066376fce8dd1fb19a0a1a3
Nikunj Thakur <nikunjrt@icloud.com>
2025-07-04 13:24:22 -0400

    Initial Commit

---
+++ Intuition/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Intuition
+//
+//  Created by Thakur, Nikunj on 2025-07-04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Intuition/IntuitionApp.swift
@@ -0,0 +1,17 @@
+//
+//  IntuitionApp.swift
+//  Intuition
+//
+//  Created by Thakur, Nikunj on 2025-07-04.
+//
+
+import SwiftUI
+
+@main
+struct IntuitionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
